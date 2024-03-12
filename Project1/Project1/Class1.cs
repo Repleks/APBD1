7a3d630ac5109fc3f184a56d10fb3bc412f12ed8
Piotr Suwara <s28023@pjwstk.edu.pl>
2024-03-12 18:57:01 +0100

    feature-average

--- Project1/Project1/Class1.cs
+++ Project1/Project1/Class1.cs
@@ -8,4 +8,15 @@ public class Class1
         Console.WriteLine(3 + 3);
         Console.Write("I will print on the same line.");
     }
+
+    static double AverageCounter(int[] arr)
+    {
+        double sum = 0;
+        for (int a = 0; a < arr.Length; a++)
+        {
+            sum += arr[a];
+        }
+        sum = sum / arr.Length;
+        return sum;
+    }
 }
